c735032ae8db3ae58332d839b071b1c7afd6aab1
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-09 08:44:19 +0700

    [r] Fix alignment in models/book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,11 @@
 class Book < ActiveRecord::Base
   paginates_per 10
   max_paginates_per 50
-	validates :title, :description, :image_url, :unit_price, :published_date, presence: true
-	validates :unit_price, numericality: { greater_than_or_equal_to: 0.01 }
-	validates :title, uniqueness: true
-	validates :image_url, allow_blank: true, format: {
-	  with: %r{\.(gif|jpg|png|jpeg)\Z}i,
-	  message: 'must be a URL for GIF, JPG, JPEG or PNG image'
-	}
+  validates :title, :description, :image_url, :unit_price, :published_date, presence: true
+  validates :unit_price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png|jpeg)\Z}i,
+    message: 'must be a URL for GIF, JPG, JPEG or PNG image'
+  }
 end
