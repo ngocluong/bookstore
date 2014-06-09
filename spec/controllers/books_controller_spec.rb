5549768e15a62e634a635dac20ebde52a39973ee
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-09 10:24:59 +0700

    [r] Modify and add more context for books_controller_spec.rb

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,31 +1,42 @@
 require 'spec_helper'
 
 describe BooksController do
-
-  context 'GET index' do
-    
-    context 'Store has less than 10 books' do
-      let!(:books) { create_list :book, 5 }
-      
-      before do
-        get :index
-      end
-
-      it 'render all books' do
-        expect(assigns[:books]).to eq books
-      end
-    end
+  def paginated_books_array(options = {})
+    Kaminari.paginate_array(books).page(options.fetch(:page, 0)).per(options.fetch(:per_page, per_page))
   end
 
-  context 'Store has more than 10 books' do
-    let!(:books) { create_list :book, 20 }
-    
+  context 'GET index' do
+    let!(:books) { create_list :book, per_page * 2 }
+    let(:per_page) { Book.default_per_page }
+
     before do
-      get :index  
+      get :index, params
     end
 
-    it 'render first 10 book' do
-      expect(assigns[:books]).to eq Book.first(10)
+    context 'not specify page' do
+      let(:params) { {} }
+
+      it 'assigns paginated books' do
+        expect(assigns[:books]).to eq(paginated_books_array)
+      end
+    end
+
+    context 'specify page' do
+      let(:page) { 2 }
+      let(:params) { { page: page } }
+
+      it 'assigns paginated books' do
+        expect(assigns[:books]).to eq(paginated_books_array(page: page))
+      end
+    end
+
+    context 'change per page' do
+      let(:per_page) { 15 }
+      let(:params) { { per_page: per_page} }
+
+      it 'assigns paginated books' do
+        expect(assigns[:books]).to eq(paginated_books_array(per_page: per_page))
+      end
     end
   end
 end
