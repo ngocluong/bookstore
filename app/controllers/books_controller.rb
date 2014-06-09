b981b5d3a14e299f2f5d108adf878c29c19173aa
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-09 10:21:32 +0700

    [r] Remove load all books in books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all.page(params[:page]).per(params[:per_page])
+    @books = Book.page(params[:page]).per(params[:per_page])
   end
 end
