c97e5c16b2623d0baee9f8100d8cf9986de75203
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-09 18:09:42 +0700

    [r] Modify default books per page in spec test

--- spec/controllers/store_controller_spec.rb
+++ spec/controllers/store_controller_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe BooksController do
 
   context 'GET index' do
-    
-    context 'Store has less than 10 books' do
+
+    context 'Store has less than 9 books' do
       let!(:books) { create_list :book, 5 }
-      
+
       before do
         get :index
       end
@@ -17,15 +17,15 @@ describe BooksController do
     end
   end
 
-  context 'Store has more than 10 books' do
+  context 'Store has more than 9 books' do
     let!(:books) { create_list :book, 20 }
-    
+
     before do
-      get :index  
+      get :index
     end
 
-    it 'render first 10 book' do
-      expect(assigns[:books]).to eq Book.first(10)
+    it 'render first 9 book' do
+      expect(assigns[:books]).to eq Book.first(9)
     end
   end
 end
