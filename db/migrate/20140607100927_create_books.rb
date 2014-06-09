f973d7561dab056a63588903fe940b53e7951088
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-09 08:43:47 +0700

    [r] Add more default values for book attributes

--- db/migrate/20140607100927_create_books.rb
+++ db/migrate/20140607100927_create_books.rb
@@ -3,11 +3,11 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
       t.string :title
       t.text :description
-      t.string :image_url
+      t.string :image_url, default: 'book.png'
       t.string :author_name, default: 'N/A'
       t.string :publisher_name, default: 'N/A'
       t.date :published_date
-      t.float :unit_price
+      t.float :unit_price, default: 0.01
       t.integer :total_rating_value, default: 0
       t.integer :total_rating_count, default: 0
 
