f46452006cca56a5b83a2627425f92382e9c100b
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-09 08:45:02 +0700

    [r] Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 20140607100927) do
   create_table "books", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "image_url"
+    t.string   "image_url",          default: "book.png"
     t.string   "author_name",        default: "N/A"
     t.string   "publisher_name",     default: "N/A"
     t.date     "published_date"
-    t.float    "unit_price"
+    t.float    "unit_price",         default: 0.01
     t.integer  "total_rating_value", default: 0
     t.integer  "total_rating_count", default: 0
     t.datetime "created_at"
