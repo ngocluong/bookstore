83c2ac4b1e6eac05777878f80b90f82e430f5b97
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-10 18:10:17 +0700

    [r] Hide public and private key for recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 # TODO - To be removed
 
 Recaptcha.configure do |config|
-  config.public_key  = '6Lcov_QSAAAAAHYa8wYkrHO-it2KLAdb9tf8TX4e'
-  config.private_key = '6Lcov_QSAAAAAO4pxt8qf2WxBtGn3Hh4dIjXemUI'
+  config.public_key  = ENV['CAPTCHA_PUBLIC_KEY']
+  config.private_key = ENV['CAPTCHA_PRIVATE_KEY']
 end
