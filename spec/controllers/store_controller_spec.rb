ebe97b6a0bcf6d753c629a090e4b94689dc70aaa
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-10 08:27:32 +0700

    [r] Get default number of book per page from model

--- spec/controllers/store_controller_spec.rb
+++ spec/controllers/store_controller_spec.rb
@@ -4,7 +4,7 @@ describe BooksController do
 
   context 'GET index' do
 
-    context 'Store has less than 9 books' do
+    context 'not pagination' do
       let!(:books) { create_list :book, 5 }
 
       before do
@@ -17,15 +17,16 @@ describe BooksController do
     end
   end
 
-  context 'Store has more than 9 books' do
-    let!(:books) { create_list :book, 20 }
+  context 'pagination' do
+    let(:per_page) { Book.default_per_page }
+    let!(:books) { create_list :book, per_page * 2 }
 
     before do
       get :index
     end
 
-    it 'render first 9 book' do
-      expect(assigns[:books]).to eq Book.first(9)
+    it 'assigns paginated books' do
+      expect(assigns[:books]).to eq Book.first(per_page)
     end
   end
 end
