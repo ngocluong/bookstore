caab37714a78cc321abc1d1dc97fff47204476f6
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-10 11:22:39 +0700

    [f] Modify Faker in Factory Girl to generate meaningful book name and random rating value

--- spec/factories/book_factory.rb
+++ spec/factories/book_factory.rb
@@ -2,14 +2,14 @@
 
 FactoryGirl.define do
   factory :book do
-    sequence (:title) { |n| "#{n} #{Faker::Lorem.word}" }
+    sequence (:title) { |n| "#{n} #{Faker::Name.name}" }
     description { Faker::Lorem.sentence }
     image_url "default.png"
-    author_name { Faker::Lorem.word }
+    author_name { Faker::Name.name }
     sequence(:publisher_name) { Faker::Lorem.word }
     sequence(:published_date)  { |n| n.year.ago.to_date }
     sequence(:unit_price)  { |n| n * 100 }
-    sequence(:total_rating_value)  { 2 }
+    sequence(:total_rating_value)  { rand(0..5) }
     sequence(:total_rating_count)  { 3 }
   end
 end
