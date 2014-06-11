d811dfddfa2851bfdbb4ec83c2aad0f2dafcf762
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-11 09:36:31 +0700

    [r] Config mailer in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,11 +68,10 @@ BookStore::Application.configure do
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
+      address:              ENV['ADDRESS'],
       port:                 587,
-      domain:               'gmail.com',
-      user_name:            ENV['USERNAME_GMAIL'],
-      password:             ENV['PASSWORD_GMAIL'],
+      user_name:            ENV['MANDRILL_USERNAME'],
+      password:             ENV['MANDRILL_APIKEY'],
       authentication:       'plain',
       enable_starttls_auto: true  }
 
