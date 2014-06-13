8f01a758ae264726dc8c8194c41c853e30d64be9
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-13 18:06:17 +0700

    [r] Add comment button and routing for view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,14 @@
 class Comment < ActiveRecord::Base
-
   belongs_to :user
   belongs_to :book
+  # after_save :update_rating_average
+
   validates :rating, numericality: { greater_than_or_equal_to: 0 }
   validates :content, presence: true
+
+  private
+  def update_rating_average
+    product = Product.find(user_id)
+    product.total_rating_value = (rating + product.total_rating_value) / (total_rating_count + 1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 BookStore::Application.routes.draw do
   resources :books, only: [:index, :show]
   resources :search, only: [:index]
+  resources :comments, only: [:new, :create]
+
   get "store/index"
   devise_for :users, controllers: { sessions: 'sessions', registrations: 'registrations', passwords: "passwords" }
 
