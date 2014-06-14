daf4cb3bcaddade07fc8fcf9afe63f33e9e6c14d
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-14 17:37:45 +0700

    [r] Config comment controller to receive rating param

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,34 @@
 class CommentsController < ApplicationController
   layout false
   before_action :authorize
+  before_action :normalize_params, only: [:create]
 
   def new
     @comment = Comment.new
   end
 
   def create
-    @comment = Comment.new(comment_params.merge(rating: params[:rating], user_id: current_user.id))
-    @book_id = params[:book_id]
+    @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to(book_path(notice: 'Thank you for your contribution', id: @book_id))
+      redirect_to book_path(@comment.book), notice: 'Thank you for your contribution'
     else
-      render action: 'new'
+      redirect_to book_path(params[:comment][:book_id]), notice: @comment.errors.full_messages.to_sentence
     end
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:rating, :content ,:book_id, :user_id)
-    end
+  def comment_params
+    params.require(:comment).permit(:content ,:book_id, :user_id, :rating)
+  end
 
-    def authorize
-      unless user_signed_in?
-        render text: 'You need to sign in inorder to make new comment'
-      end
+  def authorize
+    unless user_signed_in?
+      render text: 'You need to sign in inorder to make new comment'
     end
+  end
+
+  def normalize_params
+    params[:comment][:rating] ||= params[:comment].delete(:score)
+    params[:user_id] = current_user.id
+  end
 end
