7724f21640481979d266822b420ccfb4bedf1515
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-16 16:55:54 +0700

    [r] Calculate total price of each line item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
   belongs_to :book
   belongs_to :cart
+
+  def total_price
+    book.unit_price * quantity
+  end
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -3,4 +3,12 @@ require 'spec_helper'
 describe LineItem do
   it { should belong_to :cart }
   it { should belong_to :book }
+
+  context '#total_price' do
+    let(:line_item) { create :line_item }
+
+    it 'returns correct total price value' do
+      expect(line_item.total_price).to eq line_item.quantity * line_item.book.unit_price
+    end
+  end
 end
