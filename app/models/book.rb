0f0d85f8494edb91c3690057259f4ad5ab339414
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-16 10:51:19 +0700

    [r] Re-arrange book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,14 +2,15 @@ class Book < ActiveRecord::Base
   include PgSearch
   pg_search_scope :search, against: [:title, :author_name]
 
+  paginates_per 9
+  max_paginates_per 50
+  PAGINATION_OPTIONS = [9, 12 , 15, 18]
+
   has_many :line_items
   has_many :comments, -> { order(id: :desc) }, dependent: :destroy
   has_many :category_books, dependent: :destroy
   has_many :categories, through: :category_books
 
-  paginates_per 9
-  max_paginates_per 50
-  PAGINATION_OPTIONS = [9, 12 , 15, 18]
 
   validates :title, :description, :image_url, :unit_price, :published_date, presence: true
   validates :unit_price, numericality: { greater_than_or_equal_to: 0.01 }
