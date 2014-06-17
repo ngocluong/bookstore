2ef224915fb594ab5b72fb38f80a5fbcd7cd7484
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-17 10:56:05 +0700

    [r] Validate name and sort_order in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
+  validates :sort_order, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+
   has_many :category_books
   has_many :books, through: :category_books
 end
