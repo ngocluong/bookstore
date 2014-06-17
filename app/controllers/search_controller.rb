6aa10f7b54aade1cbb57d2f0e0fae66aff337b75
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-17 09:17:36 +0700

    [r] Handle when changing per_page attribute in index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
+  before_filter :validate_query, only: [:index]
+
   def index
     @books = BookSearcher.new(params.slice(:q, :category_id, :page, :per_page)).result
 
@@ -6,6 +8,13 @@ class SearchController < ApplicationController
       redirect_to books_path, notice: 'Can not find books which have title or author like this'
     else
       render '/books/index'
+    end
+  end
+
+  private
+  def validate_query
+    if params[:category_id].blank? && params[:q].blank?
+      redirect_to books_path(per_page: params[:per_page])
     end
   end
 end
