194d1b9fd449f85136fb380cb7dcdbf22ed46b00
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-20 15:43:08 +0700

    [r] Add permit_params in to admin book controller

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,6 +1,10 @@
 ActiveAdmin.register Book do
+  permit_params :title, :description, :image_url,
+                :author_name, :publisher_name,
+                :published_date, :unit_price,
+                :total_rating_value, :total_rating_count
 
-  
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -13,5 +17,5 @@ ActiveAdmin.register Book do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
 end
