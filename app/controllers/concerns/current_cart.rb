24236624731e220ca325f7c59c84fd392424d777
Ngoc Luong <Ngoc.Luong@eastagile.com>
2014-06-22 19:44:33 +0700

    [r] Fix indentation and unnecessary ()

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -2,11 +2,11 @@ module CurrentCart
   extend ActiveSupport::Concern
 
   private
-    def set_cart
-      @cart = Cart.find_by_code(session[:cart_code])
-      if @cart.blank?
-        @cart = Cart.create()
-        session[:cart_code] = @cart.code
-      end
+  def set_cart
+    @cart = Cart.find_by_code(session[:cart_code])
+    if @cart.blank?
+      @cart = Cart.create
+      session[:cart_code] = @cart.code
     end
+  end
 end
