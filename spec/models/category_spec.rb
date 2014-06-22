269d42ffbb50e04432acbb2040cba129de884fb0
Ngoc Luong <Ngoc.Luong@eastagile.com>
2014-06-22 19:50:30 +0700

    [r] Add dependent destroy between category and category_book relationship

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Category do
   it { should validate_numericality_of(:sort_order).is_greater_than_or_equal_to(0) }
   it { should validate_presence_of(:name) }
-  it { should have_many :category_books }
+  it { should have_many(:category_books).dependent(:destroy) }
   it { should have_many(:books).through(:category_books) }
 end
