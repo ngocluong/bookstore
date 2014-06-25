9cbd98f7b41012723ad7423078d8bcaae7434307
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-25 15:44:47 +0700

    [r] Modify delete comment cache function

--- app/service/comment/cachier.rb
+++ app/service/comment/cachier.rb
@@ -17,6 +17,7 @@ class Comment
 
     def cache_comment
       Rails.cache.fetch(cache_key) do
+        add_comment_cache_key
         paginated_comments = book.comments.page(page).includes(:user)
         paginated_info =  OpenStruct.new(current_page: paginated_comments.current_page, total_pages: paginated_comments.total_pages, per_page: paginated_comments.limit_value)
         {
@@ -27,12 +28,35 @@ class Comment
     end
 
     def destroy
-      Rails.cache.delete(cache_key) if Rails.cache.read(cache_key).present?
+      if Rails.cache.read(comment_list_key).present?
+        Rails.cache.read(comment_list_key).each do |comment_key|
+          Rails.cache.delete(comment_key) if comment_key.start_with?("comment_#{book.id}")
+        end
+        Rails.cache.write(comment_list_key, delete_comment_cache_keys)
+      end
     end
 
     private
+    def add_comment_cache_key
+      Rails.cache.write(comment_list_key, (comment_cache_keys << cache_key).uniq)
+    end
+
+    def delete_comment_cache_keys
+      Rails.cache.read(comment_list_key).delete_if do |comment_key|
+        comment_key.start_with?("comment_#{book.id}") ? true : false
+      end
+    end
+
+    def comment_cache_keys
+      Rails.cache.fetch(comment_list_key) { [] }
+    end
+
     def cache_key
       "comment_#{book.id}_#{page}"
+    end
+
+    def comment_list_key
+      'comment_cache_list'
     end
   end
 end
