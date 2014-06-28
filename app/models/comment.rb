107ccc841cc3e4880c223c54635f5e192501ae49
Ngoc Luong <ngoc.luong@eastagile.com>
2014-06-28 16:47:20 +0700

    [r] Refactor comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,16 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
-  after_save :update_total_rating
 
   paginates_per 9
   max_paginates_per 50
 
-  after_update :clear_comment_cache
-  after_create :clear_comment_cache
+  after_create :update_total_rating, :clear_comment_cache
 
   validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :content, presence: true
+  validates :content, :user, :book, presence: true
 
   private
   def update_total_rating
